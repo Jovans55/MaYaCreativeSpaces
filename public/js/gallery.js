fd9247512c83cc8a45f4c1e82cf86c798a418f3a
Jovan <jovanstosic012@gmail.com>
2024-11-21 16:35:12 -0600

    Fonts added pattern updates to pages and stlying

--- public/js/gallery.js
+++ public/js/gallery.js
@@ -1,32 +1,93 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+let slideIndexSingle = 1;
+let slideIndexKentucky = 1;
+let slideIndexCondo = 1;
+showSlidesSingle(slideIndexSingle);
+showSlidesKentucky(slideIndexKentucky);
+showSlidesCondo(slideIndexCondo);
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+function plusSlidesSingle(n) {
+    showSlidesSingle(slideIndexSingle += n);
 }
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
+function plusSlidesKentucky(n) {
+    showSlidesKentucky(slideIndexKentucky += n);
 }
 
-function showSlides(n) {
-  console.log("ran");
+function plusSlidesCondo(n) {
+    showSlidesCondo(slideIndexCondo += n);
+}
+
+function currentSlideSingle(n) {
+    showSlidesSingle(slideIndexSingle = n);
+}
+
+function currentSlideKentucky(n) {
+    showSlidesKentucky(slideIndexKentucky = n);
+}
+
+function currentSlideCondo(n) {
+    showSlidesCondo(slideIndexCondo = n);
+}
+
+function showSlidesSingle(n) {
   let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("column");
-  let captionText = document.getElementById("caption");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  let slides = document.getElementsByClassName("mySlidesSingle");
+  let dots = document.getElementsByClassName("columnSingle");
+  let captionText = document.getElementById("captionSingle");
+  if (n > slides.length) {slideIndexSingle = 1}
+  if (n < 1) {slideIndexSingle = slides.length}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace("active", "");
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  slides[slideIndexSingle-1].style.display = "block";
+  dots[slideIndexSingle-1].className += " active";
 
   let imgElement = slides[n - 1].querySelector("img");
   
   captionText.innerHTML = imgElement.alt;
+}
+
+function showSlidesKentucky(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlidesKentucky");
+    let dots = document.getElementsByClassName("columnKentucky");
+    let captionText = document.getElementById("captionKentucky");
+    if (n > slides.length) {slideIndexKentucky = 1}
+    if (n < 1) {slideIndexKentucky = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace("active", "");
+    }
+    slides[slideIndexKentucky-1].style.display = "block";
+    dots[slideIndexKentucky-1].className += " active";
+  
+    let imgElement = slides[n - 1].querySelector("img");
+    
+    captionText.innerHTML = imgElement.alt;
+}
+
+  function showSlidesCondo(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlidesCondo");
+    let dots = document.getElementsByClassName("columnCondo");
+    let captionText = document.getElementById("captionCondo");
+    if (n > slides.length) {slideIndexCondo = 1}
+    if (n < 1) {slideIndexCondo = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace("active", "");
+    }
+    slides[slideIndexCondo-1].style.display = "block";
+    dots[slideIndexCondo-1].className += " active";
+  
+    let imgElement = slides[n - 1].querySelector("img");
+    
+    captionText.innerHTML = imgElement.alt;
 }
